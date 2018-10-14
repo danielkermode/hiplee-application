cc0b5d32068d3d0fc8cdea1f19b91453ede03041
Dan <danielkermode@hotmail.co.nz>
2018-10-15 12:05:12 +1300

    Styling adjustments

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Button = props => (
-  <button onClick={props.onClick}>
-  {props.buttonText}
-  </button>
+  <div className='buttoncontainer'>
+    <button onClick={props.onClick}>
+    {props.buttonText}
+    </button>
+  </div>
 );
 
 export default Button;
